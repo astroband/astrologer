5639ed449c64d49a9d90981143ce09abcc2e812a
Viktor Sokolov <gzigzigzeo@gmail.com>
2019-04-23 21:14:06 +0400

    NewTransaction

--- ingest/main.go
+++ ingest/main.go
@@ -1,6 +1,7 @@
 package ingest
 
 import (
+	"encoding/hex"
 	"strconv"
 	"time"
 
@@ -8,7 +9,8 @@ import (
 )
 
 var (
-	ledgerShift = 32
+	ledgerShift  = 32
+	txIndexShift = 12
 )
 
 // // LedgerHeader represents json-serializable struct for LedgerHeader to index
@@ -70,8 +72,9 @@ type Ledger struct {
 // Transaction represents ledger transaction with all nested data
 type Transaction struct {
 	ID         string
-	Index      int
+	Index      byte
 	Order      int64
+	FeeCharged int64
 	ResultCode int
 	Successful bool
 }
@@ -87,28 +90,42 @@ type Source struct {
 }
 
 // NewTransaction returns transaction representation
-func NewTransaction(t *xdr.TransactionEnvelope) {
+func NewTransaction(l *Ledger, n byte, t *xdr.TransactionEnvelope, r *xdr.TransactionResultPair) (tx Transaction) {
+	tx.ID = decodeHash(r.TransactionHash)
+	tx.Index = n
+	tx.Order = Order(l.Header.LedgerSeq, n)
+	tx.ResultCode = int(r.Result.Result.Code)
+	tx.Successful = r.Result.Result.Code == xdr.TransactionResultCodeTxSuccess
+	tx.FeeCharged = int64(r.Result.FeeCharged)
 
+	return tx
 }
 
 // NewLedger return ledger struct filled out with all nested data
 func NewLedger(s Source) (l *Ledger) {
 	l = &Ledger{
 		ID:           strconv.Itoa(int(s.L.LedgerSeq)),
-		Order:        Order(s.L.LedgerSeq),
+		Order:        Order(s.L.LedgerSeq, 0),
 		CloseTime:    s.CloseTime,
 		Header:       s.L,
 		Transactions: make([]Transaction, len(s.Tx)),
 	}
 
-	for n, tx := range s.Tx {
-		//l[n] = NewTransaction()
+	for index, tx := range s.Tx {
+		r := s.R[index]
+		l.Transactions[index] = NewTransaction(l, byte(index), tx, r)
 	}
 
 	return l
 }
 
 // Order returns order fields (see horizon's toid)
-func Order(ledger xdr.Uint32) int64 {
-	return int64(ledger) << uint(ledgerShift)
+func Order(ledger xdr.Uint32, index byte) int64 {
+	f := int64(ledger) << uint(ledgerShift)
+	f = f | (int64(index) << uint(txIndexShift))
+	return f
+}
+
+func decodeHash(h xdr.Hash) string {
+	return hex.EncodeToString(h[:])
 }
