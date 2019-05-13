c47a6b58959a10814bdc818b776cf6c0f78f2b5e
Viktor Sokolov <gzigzigzeo@gmail.com>
2019-05-13 17:32:05 +0400

    Minor

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+echo "Deploying Astrograph-chronicler..."
+echo "Building..."
 GOOS=linux GOARCH=amd64 go build -o dist/stellar-core-export -v
+
+echo "Uploading..."
 scp ./dist/stellar-core-export $USER@astrograph.evilmartians.io:/home/gzigzigzeo/stellar-core-export
+
+echo "Restarting..."
+# cp /home/gzigzigzeo/stellar-core-export /home/deploy/
+# ssh $USER@astrograph.evilmartians.io
