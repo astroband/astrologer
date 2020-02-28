905729ad1a1cfa310f6d7450091f4196bf33c924
Sergey Nebolsin <nebolsin@gmail.com>
2020-02-28 06:48:30 +0300

    Bring back a few changes which escaped the previous commit

--- main.go
+++ main.go
@@ -12,19 +12,19 @@ func main() {
 	kingpin.Version(cfg.Version)
 	commandName := kingpin.Parse()
 
-	esClient := es.Connect((*cfg.EsUrl).String())
+	esClient := es.Connect((*cfg.EsURL).String())
 
 	var command cmd.Command
 
 	switch commandName {
 	case "stats":
-		dbClient := db.Connect(*cfg.DatabaseUrl)
+		dbClient := db.Connect(*cfg.DatabaseURL)
 		command = &cmd.StatsCommand{ES: esClient, DB: dbClient}
 	case "create-index":
 		config := cmd.CreateIndexCommandConfig{Force: *cfg.ForceRecreateIndexes}
 		command = &cmd.CreateIndexCommand{ES: esClient, Config: config}
 	case "export":
-		dbClient := db.Connect(*cfg.DatabaseUrl)
+		dbClient := db.Connect(*cfg.DatabaseURL)
 		config := cmd.ExportCommandConfig{
 			Start:      cfg.Start,
 			Count:      *cfg.Count,
@@ -34,7 +34,7 @@ func main() {
 		}
 		command = &cmd.ExportCommand{ES: esClient, DB: dbClient, Config: config}
 	case "ingest":
-		dbClient := db.Connect(*cfg.DatabaseUrl)
+		dbClient := db.Connect(*cfg.DatabaseURL)
 		command = &cmd.IngestCommand{ES: esClient, DB: dbClient}
 	case "es-stats":
 		command = &cmd.EsStatsCommand{ES: esClient}
